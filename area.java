66c66c6aef17ccf2216c8a6b6ef072cc24b76b3b
MudhithaK <mudhithakalpani08@gmail.com>
2024-06-06 19:13:30 +0530

    Add files via upload

---
+++ area.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+class area{
+
+	
+	public static void main(String[] args){
+		/*double a,b,total,price;
+		a=10;
+		b=15;
+
+		double num1= 8.2;
+		int num2= 6;
+
+		double area= a*b;
+		System.out.println("Area of the rectangle :"+area);
+
+
+		System.out.println("First number ="+num1 +"second number="+num2);
+		
+		total=2182.50;
+		price = total/15;
+		System.out.println("Unit price ="+price);
+
+
+		char ch= 48;
+		for(int i=0;i<128;i++){
+			System.out.println(i+"-"+ ch);
+			ch++;
+		}*/
+
+
+		/*double ts,fps,far;
+		ts=7.2;
+		fps=1100;
+		far=1100*7.2;
+		
+		System.out.println("Distance between listener from lightning strike="+far+"feet");*/
+
+
+		/*System.out.println("Fname \t Lname \t M\\F \n Jane \t \" Done\"  F \n \"Ann\" \t Smith \t F \n Bob'd \t Alwis \t M");*/
+
+
+		Scanner scn= new Scanner(System.in);
+		double a,b;
+		System.out.println("Enter the first number :");
+		a= scn.nextDouble();
+
+		System.out.println("Enter the second number :");
+		b= scn.nextDouble();
+
+		System.out.println("Addition of the numbers ="+(a+b));
+		System.out.println("Reduction of the numbers ="+(a-b));
+		System.out.println("Multiplication of the numbers = "+(a*b));
+		System.out.println("Division of the numbers ="+(a/b));
+		System.out.println("Modulus of the numbers ="+(a%b));
+
+		
+		
+
+		
+		
+  }
+
+
+}
